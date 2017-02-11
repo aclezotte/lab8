0fbf7909048e50c5cc672d66ff4f0ba44312b3a5
Adam Lezotte <adam@lezotte.me>
2017-02-11 15:57:54 -0500

    remove old function

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-/*function shoppingList (items, prices) {
-    var total = 0;
-    
-    for ( i = 0; i < items.length; i++) {
-        console.log ( items[i] + ": $" + prices[i] );
-        total += prices[i];
-    }
-    
-    console.log ( "Total: $" + total );
-}
-
-var items = [
-    "Milk",
-    "Bread",
-    "Apples",
-    "Spinach"
-];
-
-var prices = [
-    2.49,
-    3.99,
-    4.49,
-    1.99
-];
-
-shoppingList (items, prices);*/
-
 function shoppingListObjects (list) {
     var total = 0;
     
