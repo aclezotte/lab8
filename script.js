51e21f8e3e161e5fb0372f8ed4b143de333360e5
Adam Lezotte <adam@lezotte.me>
2017-02-14 20:05:48 -0500

    remove object names

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function shoppingListObjects (list) {
     var total = 0;
     
-    for ( i = 0; i < list.length; i++ ) {
-        console.log ( list[i].name + ": $" + list[i].price );
+    for(i = 0; i < list.length; i++) {
+        console.log(list[i].name + ": $" + list[i].price);
         total += list[i].price;
     }
     
@@ -10,10 +10,10 @@ function shoppingListObjects (list) {
 }
 
 var groceryList = [
-    milk = {name: "Milk", price: 2.49},
-    bread = {name: "Bread", price: 3.99},
-    apples = {name: "Apples",price: 4.49,},
-    spinach = {name: "Spinach", price: 1.99,}
+    {name: "Milk", price: 2.49},
+    {name: "Bread", price: 3.99},
+    {name: "Apples",price: 4.49,},
+    {name: "Spinach", price: 1.99,}
 ];
 
-shoppingListObjects (groceryList);
+shoppingListObjects(groceryList);
